0decfe03c3e245943078beececb3ce317455ab89
jimmy1217 <jimmy741217@gmail.com>
2019-06-11 00:44:17 +0800

    styled-components

--- components/RootPageComponent/RootPageComponent.js
+++ components/RootPageComponent/RootPageComponent.js
@@ -1,18 +1,39 @@
 import React, { Component } from "react";
 import { inject } from "mobx-react";
 import { observer } from "mobx-react-lite";
+import styled from "styled-components";
+
+const Contain = styled.div`
+  display: flex;
+  flex:1;
+  flex-direction:column;
+`;
+
+const Card = styled.div`
+  display: inline-flex;
+  flex: 1;
+  justify-content:center;
+  box-shadow:0 0px 3px 0 #ddd;
+  margin:20px;
+`;
+
+const Reset = styled.button`
+    margin:20px;
+    padding:20px 0;
+    background-color: transparent;
+`
 
 const RootPage = ({ store }) => {
   const { reset, list } = store.RootPageStore;
   return (
-    <div>
+    <Contain>
       {list.map(item => (
-        <ul key={item.id}>
-          <li>{item.username}</li>
-        </ul>
+        <Card key={item.id}>
+          <h6>{item.username}</h6>
+        </Card>
       ))}
-      <button onClick={() => reset()}>reset value</button>
-    </div>
+      <Reset onClick={() => reset()}>reset value</Reset>
+    </Contain>
   );
 };
 
