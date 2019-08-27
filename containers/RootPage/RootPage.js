2f8ee9462902d8fd8f8988e86eddf0912e4161a0
jimmy1217 <jimmy741217@gmail.com>
2019-08-27 11:20:17 +0800

    data

--- containers/RootPage/RootPage.js
+++ containers/RootPage/RootPage.js
@@ -5,11 +5,11 @@ import { callGetUser } from "@api";
 class RootPage extends Component {
   static async getInitialProps({ mobxStore }) {
     try {
-      const { assignData } = mobxStore.RootPageStore;
-      assignData({ isFetching: true });
       const res = await callGetUser();
-      assignData({ isFetching: false });
-      assignData({ list: res.data, didInvalidate: false });
+      mobxStore.RootPageStore.assignData({
+        list: res.data.map(item => ({ id: item.id, username: item.username })),
+        didInvalidate: false
+      });
     } catch (error) {
       console.log(error);
     }
