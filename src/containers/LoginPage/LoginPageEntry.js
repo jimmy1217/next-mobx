8b3bc315d58203a614dd9ae5433792f56b6e1f26
jimmy <jimmy741217@gmail.com>
2019-12-26 01:14:23 +0800

    docker file

--- src/containers/LoginPage/LoginPageEntry.js
+++ src/containers/LoginPage/LoginPageEntry.js
@@ -4,29 +4,50 @@ import { useLocalStore, useObserver } from "mobx-react";
 export default () => {
   const store = useLocalStore(() => ({
     isLogin: false,
+    account: "",
+    password: "",
     toggleLogin() {
       store.isLogin = !store.isLogin;
+    },
+    onChangeStore(e) {
+      store[e.target.name] = e.target.value;
     }
   }));
 
-  return useObserver(() => (
-    <LoginWrapper>
-      <div className={`login_container ${store.isLogin ? "login" : ""}`}>
-        <div className={`top_card ${store.isLogin ? "fixed_top" : ""}`}>
-          <h4>Login</h4>
+  return useObserver(() => {
+    const { isLogin, account, password, toggleLogin, onChangeStore } = store;
+    return (
+      <LoginWrapper>
+        <div className={`nav_container ${isLogin ? "login" : ""}`}></div>
+        <div className={`login_container ${isLogin ? "login" : ""}`}>
+          <div className={`top_card ${isLogin ? "fixed_top" : ""}`}>
+            <h4>Login</h4>
+          </div>
+          <div className="text-field">
+            <input
+              type="text"
+              name="account"
+              value={account}
+              placeholder="ex:jimmy"
+              onChange={onChangeStore}
+            />
+          </div>
+          <div className="text-field">
+            <input
+              type="password"
+              name="password"
+              placeholder="ex:123456"
+              value={password}
+              onChange={onChangeStore}
+            />
+          </div>
+          <div className="text-field btn" onClick={store.toggleLogin}>
+            Submit
+          </div>
         </div>
-        <div className="text-field">
-          <input type="text" />
-        </div>
-        <div className="text-field">
-          <input type="password" name="" id="" />
-        </div>
-        <div className="text-field btn" onClick={store.toggleLogin}>
-          Submit
-        </div>
-      </div>
-    </LoginWrapper>
-  ));
+      </LoginWrapper>
+    );
+  });
 };
 
 const LoginWrapper = styled.div`
@@ -48,12 +69,27 @@ const LoginWrapper = styled.div`
     padding: 120px 20px 20px 20px;
     border-radius: 10px;
     background-color: rgba(255, 255, 255, 0.9);
-    transition: all 0.5s cubic-bezier(0.16, 1.3, 0, 1.01) 0.25s;
+    transition: all 0.6s cubic-bezier(0.16, 1.3, 0, 1.01) 0.25s;
     &.login {
       max-width: 100%;
       height: 100%;
       background-color: #fff;
       border-radius: 0;
+    }
+  }
+  .nav_container {
+    height: 100%;
+    width: 240px;
+    background-color: #252d3a;
+    position: absolute;
+    left: 0;
+    top: 0;
+    z-index: 3;
+    transition: transform 0.3s ease-in-out;
+    transform: translate3d(0, -100%, 0);
+    &.login {
+      transition: transform 0.3s ease-out 0.32s;
+      transform: translate3d(0, 0, 0);
     }
   }
   .top_card {
@@ -70,23 +106,27 @@ const LoginWrapper = styled.div`
     color: #fff;
     transition: all 0.3s ease;
     box-shadow: 0 12px 20px -10px rgba(115, 115, 115, 0.28),
-      0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(156, 39, 176, 0.2);
+      0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 7px 8px -5px rgba(208, 208, 208, 0.2);
     h4 {
-      letter-spacing:3px;
-      font-weight:300;
-      opacity:1;
+      letter-spacing: 3px;
+      font-weight: 300;
+      opacity: 1;
       transition: opacity 0.8s ease 0.5s;
     }
     &.fixed_top {
       position: absolute;
       width: 100%;
-      height: 100px;
+      height: 50px;
       top: 0;
       left: 0;
       transform: translate3d(0, 0, 0);
+      background: linear-gradient(60deg, #fff, #fff);
       border-radius: 0;
+      z-index: 3;
+      box-shadow:none;
+      border-bottom: 1px solid #ddd;
       h4 {
-        opacity:0;
+        opacity: 0;
         transition: opacity 0.3s ease 0s;
       }
     }
@@ -94,6 +134,7 @@ const LoginWrapper = styled.div`
   .text-field + .text-field {
     margin-top: 10px;
   }
+
   input {
     font-size: 16px;
     width: 100%;
