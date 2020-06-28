d05c5d2ef6fb4bba4e17e1589ad86e06ec15fcf9
jimmy <jimmy741217@gmail.com>
2020-06-29 00:06:51 +0800

    favicon

--- pages/_document.js
+++ pages/_document.js
@@ -24,6 +24,13 @@ export default class MyDocument extends Document {
       <html>
         <Head>
           {this.props.styleTags}
+          <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/example/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/example/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/example/favicon-16x16.png" />
+          <link rel="manifest" href="/static/favicons/example/site.webmanifest" />
+          <link rel="mask-icon" href="/static/favicons/example/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#2d89ef" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
 
         <body>
